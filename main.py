91d37fffd7f2c3cafd006233cf17b0d174dc4184
Billy Euceda <billymm314@gmail.com>
2024-04-22 13:32:18 -0700

    rm css

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 import boto3
 
+app = Flask(__name__)
+
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("nobel")
-
-app = Flask(__name__)
 
 @app.route("/")
 def home():
